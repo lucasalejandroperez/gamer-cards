87178f1e58d59aedc3226ed7f3c279c35888a463
Lucas Perez <lucas.alejandro.perez@gmail.com>
2022-05-09 23:45:21 -0300

    NFT contract added and completed, stil without testing

--- src/solidity/contracts/NFT.sol
+++ src/solidity/contracts/NFT.sol
@@ -4,12 +4,16 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFT is ERC721URIStorage {
-    uint public tokenCount;
-    constructor() ERC721("Gamer Cards", "DAPP"){}
-    function mint(string memory _tokenURI) external returns(uint) {
-        tokenCount ++;
-        _safeMint(msg.sender, tokenCount);
-        _setTokenURI(tokenCount, _tokenURI);
-        return(tokenCount);
+    uint public tokenId;
+    constructor() ERC721("Gamer Cards", "ETH"){}
+    function mint(string[] memory _tokenURIS) external returns(uint[] memory) {
+        uint[] memory tokenIdsArray = new uint[](_tokenURIS.length);
+        for (uint256 i = 0; i < _tokenURIS.length; i++) {
+            tokenId++;
+            _safeMint(msg.sender, tokenId);
+            _setTokenURI(tokenId, _tokenURIS[i]);
+            tokenIdsArray[i] = tokenId;
+        }
+        return(tokenIdsArray);
     }
 }
