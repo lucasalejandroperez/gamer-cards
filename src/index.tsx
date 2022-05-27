2955776786057b419cfd88982bc7eff4827073ef
Lucas Perez <lucas.alejandro.perez@gmail.com>
2022-05-27 19:40:26 -0300

    Se agrega una primer version de la card

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from "react-redux"
 import { GamerCardsApp } from './frontend/GamerCardsApp';
 import { store } from './frontend/redux/store';
-import './frontend/index.css';
+import './frontend/styles/general.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
